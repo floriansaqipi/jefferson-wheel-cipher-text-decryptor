0c7a746f53573e0fa6ce35f3efbd4714f2f85601
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-03-19 23:31:49 +0100

    final version

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args){
-//        File fileInput = new File("src/files/input.txt");
-        File fileInput = new File("src/files/input_shqip.txt");
+//        File fileInputEnglish = new File("src/files/input.txt");
+        File fileInputAlbanian = new File("src/files/input_shqip.txt");
         File fileOutput = new File("src/files/output.txt");
 
-//        FileDecrypter fileDecrypter = new EnglishDecrypter(fileInput,fileOutput);
-        FileDecrypter fileDecrypter = new AlbanianDecrypter(fileInput,fileOutput);
+//        FileDecrypter fileDecrypter = new EnglishDecrypter(fileInputEnglish,fileOutput);
+        FileDecrypter fileDecrypter = new AlbanianDecrypter(fileInputAlbanian,fileOutput);
         fileDecrypter.performDecryption();
 
 
