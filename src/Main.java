c040f6a4bcf61fb0a413802e909a2e1a8a48278a
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-03-20 01:38:27 +0100

    production ready

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args){
 //        File fileInputEnglish = new File("src/files/input.txt");
+//        File fileInputEnglish = new File("src/files/input_small.txt");
         File fileInputAlbanian = new File("src/files/input_shqip.txt");
         File fileOutput = new File("src/files/output.txt");
 
--- src/alphabetUtils/Alphabet.java
+++ src/alphabetUtils/Alphabet.java
@@ -4,7 +4,7 @@ public class Alphabet {
     private Alphabet(){}
     public static final String englishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String englishAlphabetFrequencyOrder = "ETAOINSRHLDUCMGYWFPBVKJXZQ";
-//    public static final String englishAlphabetFrequencyOrder = "ETAOINSRHDLUCMFYWGPBVKXQJZ";
+//    public static final String englishAlphabetFrequencyOrder = "EATSOHRINDFWUBYGLCPMVKXQJZ"; //st //bgu //mp
     public static final String albanianAlphabet = "ABCÇDEËFGHIJKLMNOPQRSTUVXYZ";
     public static final String albanianAlphabetFrequencyOrder = "EËTIARNSHJUKDMOPLGVBQYFZÇCX";
 
